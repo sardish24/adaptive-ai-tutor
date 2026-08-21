1050bf7a0605e1d9d4a7acfc53813c49c62e03b7
Kaizer-sardish <f20240563@pilani.bits-pilani.ac.in>
2026-08-21 10:35:07 +0530

    Fix: Enhance WebRTC RTC_CONFIGURATION with resilient public STUN endpoints for cloud deployment

--- app.py
+++ app.py
@@ -311,6 +311,10 @@ RTC_CONFIGURATION = {
         {"urls": ["stun:stun.l.google.com:19302"]},
         {"urls": ["stun:stun1.l.google.com:19302"]},
         {"urls": ["stun:stun2.l.google.com:19302"]},
+        {"urls": ["stun:stun3.l.google.com:19302"]},
+        {"urls": ["stun:stun4.l.google.com:19302"]},
+        {"urls": ["stun:stun.services.mozilla.com"]},
+        {"urls": ["stun:global.stun.twilio.com:3478"]},
     ]
 }
 
